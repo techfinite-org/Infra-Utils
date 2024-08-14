a64175fc42844409a86cff93d2e9adc9d5227d86
Balamurugan1207 <bramachandran@techfinite.com>
2024-08-14 16:02:14 +0530

    updated install_mariadb.sh

--- install_mariadb.sh
+++ install_mariadb.sh
@@ -1,8 +1,7 @@
 # !/bin/bash
 
 sudo apt update -y
-sudo apt install mariadb-server mariadb-client -y
-sudo mysql_secure_installation
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "[mysqld]\ncharacter-set-client-handshake = FALSE\ncharacter-set-server = utf8mb4\ncollation-server = utf8mb4_unicode_ci\n\n[mysql]\ndefault-character-set = utf8mb4" >> /etc/mysql/my.cnf
+sudo apt install software-properties-common mariadb-server mariadb-client -y
+sudo echo "[mysqld]\nbind_address=0.0.0.0\ncharacter-set-client-handshake = FALSE\ncharacter-set-server = utf8mb4\ncollation-server = utf8mb4_unicode_ci\n\n[mysql]\ndefault-character-set = utf8mb4" >> /etc/mysql/my.cnf
 sudo service mariadb restart
+sudo mysql_secure_installation
